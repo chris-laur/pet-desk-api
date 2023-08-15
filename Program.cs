f6d6fd3f55dc9b7f773fabb6837441bf9012e068
Christie Laur <claur@maus.com>
2023-08-14 20:01:31 -0400

    CORS fix for hosted demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+//using Microsoft.EntityFrameworkCore;
 using PetDeskDataModels;
 using System.Text.Json;
 
@@ -6,17 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<PetDeskDb>(opt => opt.UseInMemoryDatabase("AppointmentChangeRequests"));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "PetDeskClientApp",
+                      policy  =>
+                      {
+                          policy.WithOrigins("https://chris-laur.github.io").WithMethods("GET");
+                      });
+});
+
 var app = builder.Build();
 
-var requests = app.MapGroup("/appointmentchangerequests");
-requests.MapGet("/", GetAppointmentChangeRequests);
+var requests = app.MapGroup("/api");
 
+requests.MapGet("/appointmentchangerequests", GetAppointmentChangeRequests);
+
+app.UseCors("PetDeskClientApp");
 app.Run();
 
 async Task<IResult> GetAppointmentChangeRequests(/*PetDeskDb db*/)
 {
     var requests = new List<AppointmentChangeRequest>();
-    
+
     using (var httpClient = new HttpClient())
     {
         using (var response = await httpClient.GetAsync("https://723fac0a-1bff-4a20-bdaa-c625eae11567.mock.pstmn.io/appointments"))
